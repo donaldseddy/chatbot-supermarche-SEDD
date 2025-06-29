6009f02a793b44ef48e6378341ec1787a2d91a81
Your Name <you@example.com>
2025-06-29 08:56:56 +0200

    Remove unused 'users' app and update product templates; add product addition and listing functionality with Tailwind CSS styling

--- supermarket_chatbot/settings/base.py
+++ supermarket_chatbot/settings/base.py
@@ -44,8 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Apps perso
-    'chatbot',
-    'users',
+    'chatbot'
 ]
 
 # Middleware
@@ -65,7 +64,7 @@ ROOT_URLCONF = 'supermarket_chatbot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
